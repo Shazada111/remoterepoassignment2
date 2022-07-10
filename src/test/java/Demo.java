8bb41eed14e79aa2a4b94ae6a6a2a44bcaa1fdae
xiahezhadan.wushouer <urumchi2020@gmail.com>
2022-07-10 11:14:08 -0400

    initial project states

---
+++ src/test/java/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println("this is a code base form");
+        System.out.println("project assignment2");
+    }
+}
